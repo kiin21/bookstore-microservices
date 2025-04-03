d28ad175e8533b5578ca636cb56df14ee5590a58
anh khoa <pdanhkhoa21@gmail.com>
2025-04-03 09:54:52 +0700

    feat: add wiremock server for testing

--- order-service/src/test/java/com/kiin/bookstore/orders/TestcontainersConfiguration.java
+++ order-service/src/test/java/com/kiin/bookstore/orders/TestcontainersConfiguration.java
@@ -3,12 +3,25 @@ package com.kiin.bookstore.orders;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistrar;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.utility.DockerImageName;
+import org.wiremock.integrations.testcontainers.WireMockContainer;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
+
+    static WireMockContainer wiremockServer = new WireMockContainer("wiremock/wiremock:3.5.2-alpine");
+
+    @Bean
+    WireMockContainer wiremockServer() {
+        wiremockServer.start();
+        configureFor(wiremockServer.getHost(), wiremockServer.getPort());
+        return wiremockServer;
+    }
 
     @Bean
     @ServiceConnection
@@ -21,4 +34,13 @@ class TestcontainersConfiguration {
     RabbitMQContainer rabbitContainer() {
         return new RabbitMQContainer(DockerImageName.parse("rabbitmq:latest"));
     }
+
+    @Bean
+    DynamicPropertyRegistrar dynamicPropertyRegistrar(WireMockContainer wiremockServer) {
+        return (registry) -> {
+            registry.add("orders.catalog-service-url", wiremockServer::getBaseUrl);
+        };
+    }
+
+
 }
