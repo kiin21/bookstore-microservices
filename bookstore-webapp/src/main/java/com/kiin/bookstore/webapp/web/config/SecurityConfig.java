d5fe55f70f47e75be0d46fcecb167b64d180dfaa
kiin21 <pdanhkhoa21@gmail.com>
2025-06-06 09:58:09 +0700

    fix: build all services from base image

--- bookstore-webapp/src/main/java/com/kiin/bookstore/webapp/web/config/SecurityConfig.java
+++ bookstore-webapp/src/main/java/com/kiin/bookstore/webapp/web/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.kiin.bookstore.webapp.web.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -18,7 +16,6 @@ import org.springframework.security.web.authentication.logout.LogoutSuccessHandl
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
     private final ClientRegistrationRepository clientRegistrationRepository;
 
     SecurityConfig(ClientRegistrationRepository clientRegistrationRepository) {
